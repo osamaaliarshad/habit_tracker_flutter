967ec9b73a498edb9d064a37b1d497d4de53205f
oali <oaliarshad@gmail.com>
2021-06-03 01:04:50 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/constants/app_assets.dart';
+import 'package:habit_tracker_flutter/constants/app_colors.dart';
+import 'package:habit_tracker_flutter/ui/home/home_page.dart';
+import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppAssets.preloadSVGs();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Helvetica Neue',
+      ),
+      home: AppTheme(
+        data: AppThemeData.defaultWithSwatch(AppColors.red),
+        child: HomePage(),
+      ),
+    );
+  }
+}
